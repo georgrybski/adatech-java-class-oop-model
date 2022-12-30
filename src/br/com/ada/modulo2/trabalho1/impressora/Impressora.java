f50ff606325bcf1a3fb598b481dddf027baa7e32
georgrybski <rybskigeorg@gmail.com>
2022-12-30 18:54:13 -0300

    Adicionada classe Impressora, junto com estrutura do metodo imprimirMensagemFormatada

---
+++ src/br/com/ada/modulo2/trabalho1/impressora/Impressora.java
@@ -0,0 +1,23 @@
+package br.com.ada.modulo2.trabalho1.impressora;
+
+public class Impressora {
+
+    private static int comprimentoDelinha =  82;
+
+    private Impressora() {}
+
+    public static void imprimirMensagemFormatada(String mensagem) {
+        imprimirLinhaDelimitadora();
+
+        boolean mensagemMaiorQueLinha = (mensagem.length() > comprimentoDelinha - 6);
+        if (mensagemMaiorQueLinha) {
+            imprimirStringMultiLinhasFormatada(mensagem);
+        } else {
+            imprimirLinhaIndentadaFormatada(mensagem);
+        }
+        imprimirLinhaDelimitadora();
+    }
+
+    
+
+}
