6c0643a3f88cc5758df4504e9ab8764f14dc20e9
georgrybski <rybskigeorg@gmail.com>
2023-01-07 16:55:41 -0300

    Adicionados atributos e contrutor à classe Professor

--- src/br/com/ada/modulo2/trabalho1/Professor.java
+++ src/br/com/ada/modulo2/trabalho1/Professor.java
@@ -1,4 +1,18 @@
 package br.com.ada.modulo2.trabalho1;
 
+import java.util.ArrayList;
+
 public class Professor {
+
+    private String nome;
+    private int ID;
+
+    private static ArrayList<Professor> professores;
+    private static int contadorDeID = 0;
+
+
+    private Professor(String nome) {
+        this.nome = nome;
+        this.ID = contadorDeID++;
+    }
 }
