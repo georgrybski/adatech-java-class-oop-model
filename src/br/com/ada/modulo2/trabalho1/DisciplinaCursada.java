d9ff553e4377696c47a911dbfeb545d14c64999a
Marcus-Vinicius-Oliveira <mvoliveirajus@gmail.com>
2023-01-07 13:25:25 -0300

    primeiro commit DisciplinaCursada

--- src/br/com/ada/modulo2/trabalho1/DisciplinaCursada.java
+++ src/br/com/ada/modulo2/trabalho1/DisciplinaCursada.java
@@ -1,4 +1,41 @@
 package br.com.ada.modulo2.trabalho1;
 
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+
 public class DisciplinaCursada {
+
+    Disciplina disciplina;
+
+    private ArrayList<Double> notas = new ArrayList<>();
+
+    String strDataInicial;
+
+    String strDataFinal;
+
+    public DisciplinaCursada(Disciplina disciplina, ArrayList<Double> notas, String strDataInicial, String strDataFinal) {
+        this.disciplina = disciplina;
+        this.notas = notas;
+        this.strDataInicial = strDataInicial;
+        this.strDataFinal = strDataFinal;
+    }
+
+    public boolean emCurso() {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate fimDaDisciplina = LocalDate.parse(strDataFinal, formatter);
+        LocalDate inicioDaDisciplina = LocalDate.parse(strDataInicial, formatter);
+        LocalDate dataAtual = LocalDate.now();
+
+        boolean disciplinaEmCurso =
+                (dataAtual.compareTo(fimDaDisciplina) > 0 && dataAtual.compareTo(inicioDaDisciplina) < 0);
+        return disciplinaEmCurso;
+    }
+
+
+
 }
