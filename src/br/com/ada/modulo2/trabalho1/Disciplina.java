6088f76b8ab1b7fb5ea7aa0550f67a0b42ed977d
jkmacielbr <jk.maciel.br@gmail.com>
2023-01-07 22:13:57 -0400

    implementando disciplina

--- src/br/com/ada/modulo2/trabalho1/Disciplina.java
+++ src/br/com/ada/modulo2/trabalho1/Disciplina.java
@@ -1,4 +1,29 @@
 package br.com.ada.modulo2.trabalho1;
 
+import java.util.ArrayList;
+
 public class Disciplina {
+    private String nome;
+    private ArrayList<Professor> professores = new ArrayList<>();
+
+    static private ArrayList<Disciplina> disciplinas = new ArrayList<>();
+
+    public Disciplina(String nome) {
+        this.nome = nome;
+    }
+
+    static void criarDisciplina(String nome){
+        disciplinas.add(new Disciplina(nome));
+    }
+    static void criarDisciplina(String nome, Professor professor){
+        Disciplina disciplina = new Disciplina(nome);
+        disciplinas.add(disciplina);
+        disciplina.professores.add(professor);
+    }
+
+
+
+
+
+
 }
