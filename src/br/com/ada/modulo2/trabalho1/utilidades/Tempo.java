f8ad05e12c7b09ac1ed4ff8a74112622b5ae1b86
georgrybski <rybskigeorg@gmail.com>
2023-01-08 17:04:18 -0300

    Adicionados métodos para checagem de datas e calculo de idade

--- src/br/com/ada/modulo2/trabalho1/utilidades/Tempo.java
+++ src/br/com/ada/modulo2/trabalho1/utilidades/Tempo.java
@@ -2,6 +2,7 @@ package br.com.ada.modulo2.trabalho1.utilidades;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
@@ -10,6 +11,11 @@ import java.time.format.DateTimeParseException;
  */
 public class Tempo {
 
+    /**
+     * Método que checa se a String inserida é uma data válida
+     * @param data
+     * @return boolean
+      */
     public static boolean dataValida(String data) {
         try{
             var formatador =
@@ -21,4 +27,28 @@ public class Tempo {
         }
     }
 
+
+    /**
+     * Método que retorna uma String da data atual no formato dd/mm/aaaa
+     * @return String
+     */
+    public static String dataAtual() {
+        LocalDateTime agora = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return agora.format(formatter);
+    }
+
+    /**
+     * Método que retorna o número de anos entre a data fornecida (String no formato dd/mm/aaaa)
+     * e a data atual
+     * @param dataDeNascimento
+     * @return int
+     */
+    public static int idade(String dataDeNascimento) {
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate nascimento = LocalDate.parse(dataDeNascimento, formatador);
+        LocalDate now = LocalDate.now();
+        Period idade = Period.between(nascimento, now);
+        return idade.getYears();
+    }
 }
