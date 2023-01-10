c6da5c3da58fc7ab1484198be788874109546224
georgrybski <rybskigeorg@gmail.com>
2023-01-10 17:23:23 -0300

    Refatoração de código

--- src/br/com/ada/modulo2/trabalho1/Main.java
+++ src/br/com/ada/modulo2/trabalho1/Main.java
@@ -11,55 +11,59 @@ public class Main {
         while (rodando){
             switch (Menu.menuInicial()) {
 
+                // Cadastrar Professor
                 case 1:
                     Coordenacao.contratarProfessor(Input.receberString("Insira o nome do(a) Professor(a)"));
                     break;
 
+                // Cadastrar Aluno
                 case 2:
                     Coordenacao.matricularAluno(Input.receberString("Insira o nome do(a) aluno(a)"));
                     break;
 
+                // Cadastrar Turma
                 case 3:
+                    if(Professor.getProfessores().isEmpty()) {
+                        Impressora.imprimirMensagemFormatada("Cadastre um professor antes de tentar criar uma turma");
+                    } else {
+                        Coordenacao.criarTurma(
+                                Input.receberString("Insira o nome da turma"),
+                                Professor.ID(
+                                        Input.receberInt(1, Professor.getContadorDeID(),
+                                                "Insira o ID do(a) professor(a)",
+                                                ("Valor inválido, insira um ID entre + " +
+                                                        1 + " e " + Professor.getContadorDeID())
+                                        )
+                                )
+                        );
+                    }
+
+                // Listar Professores
+                case 4:
                     Coordenacao.listarProfessores();
                     break;
 
-                case 4:
+                // Listar Alunos
+                case 5:
                     Coordenacao.listarAlunos();
                     break;
 
-                case 5:
-                    Coordenacao.criarTurma(
-                            Input.receberString("Insira o nome da turma"),
-                            Professor.ID(
-                                    Input.receberInt(1, Professor.getContadorDeID(),
-                                            "Insira o ID do(a) professor(a)",
-                                            ("Valor inválido, insira um ID entre + " +
-                                            1 + " e " + Professor.getContadorDeID())
-                                    )
-                            )
-                    );
-
+                // Listar Turmas
                 case 6:
                     Coordenacao.listarTurmas();
                     break;
 
+                // Iniciar Aula
                 case 7:
                     if(Professor.getProfessores().isEmpty()) {
                         Impressora.imprimirMensagemFormatada("Cadastre um professor antes de tentar começar uma aula");
                     } else {
-                        Menu.darAula();
+                        Menu.iniciarAula();
                     }
                     break;
 
-                    case 8:
-                    if(Professor.getProfessores().isEmpty()) {
-                        Impressora.imprimirMensagemFormatada("Cadastre um professor antes de tentar criar uma turma");
-                    } else {
-                        System.out.println(Menu.menuDeProfessores().toString());
-                    }
-                    break;
-
-                case 9:
+                // Sair
+                case 8:
                     rodando = false;
                     Impressora.imprimirMensagemFormatada("Encerrando serviço");
 
--- src/br/com/ada/modulo2/trabalho1/utilidades/Menu.java
+++ src/br/com/ada/modulo2/trabalho1/utilidades/Menu.java
@@ -1,12 +1,9 @@
 package br.com.ada.modulo2.trabalho1.utilidades;
 
-
 import br.com.ada.modulo2.trabalho1.Professor;
 import br.com.ada.modulo2.trabalho1.Turma;
 
-import java.awt.dnd.DragGestureRecognizer;
 import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 /**
  * Classe que lida com chamadas de métodos
@@ -17,12 +14,17 @@ public class Menu {
 
     private static final String[] OPCOES1 =
             {"1 - Cadastrar Professor", "2 - Cadastrar Aluno",
-            "3 - Lista de Professores", "4 - Lista de Alunos",
-            "5 - Criar Turma", "6 - Lista de Turmas", "7 - Começar Aula" ,"8 - Imprimir Professor" , "9 - Sair"};
+            "3 - Cadastrar Turma", "4 - Lista de Professores",
+            "5 - Lista de Alunos", "6 - Lista de Turmas", "7 - Iniciar Aula" ,"8 - Sair"};
 
+    private Menu() {}
+
+    public static int menuInicial() {
+        return receberInt(OPCOES1);
+    }
 
     private static String[] opcoesDeProfessores(ArrayList<Professor> professores) {
-        return professores.stream().map(Professor::toString).collect(Collectors.toList()).toArray(String[]::new);
+        return professores.stream().map(Professor::toString).toList().toArray(String[]::new);
     }
 
     public static Professor menuDeProfessores(){
@@ -31,29 +33,25 @@ public class Menu {
     }
 
     private static String[] opcoesDeTurmas(ArrayList<Turma> turmas) {
-        return turmas.stream().map(turma -> turma.getID() + " | Turma " + turma.getNome() ).collect(Collectors.toList()).toArray(String[]::new);
+        return turmas.stream().map(turma -> turma.getID() + " | Turma " + turma.getNome() ).toList().toArray(String[]::new);
     }
 
     public static Turma menuDeTurmas(ArrayList<Turma> turmas) {
         return Turma.ID(receberInt(opcoesDeTurmas(turmas)));
     }
 
-    public static void darAula() {
+    public static void iniciarAula() {
+        Impressora.imprimirMensagemFormatada("Selecione um(a) professor(a) abaixo:");
         var professor = menuDeProfessores();
         var turmas = professor.getTurmas();
         if (!turmas.isEmpty()) {
+            Impressora.imprimirMensagemFormatada("Selecione uma turma abaixo:");
             professor.darAula(menuDeTurmas(turmas));
         } else {
             Impressora.imprimirMensagemFormatada(professor.getNome() + " não está alocado a nenhuma turma");
         }
     }
 
-
-    private Menu() {}
-
-    public static int menuInicial() {
-        return receberInt(OPCOES1);
-    }
 
     /**
      * Imprime um menu e retorna valor inserido pelo usuário.
